f81f82231733b385f9e549385e569efdd43f0f00
Deadly-Stricker <sakshamsinha143@gmail.com>
2025-04-20 14:44:36 +0530

    modifications

--- DFT2.v
+++ DFT2.v
@@ -1,11 +1,13 @@
 module DFT2(
     input [7:0]in1,
     input [7:0]in2,
-    output reg [7:0]out1,
-    output reg [7:0]out2
+    output reg [7:0] real_out1,
+    // output reg [7:0]imag_out1,
+    output reg [7:0] real_out2
+    // output reg [7:0]imag_out2
 );
     always @(*) begin
-        out1<=in1+in2;
-        out2<=in1-in2;
+        real_out1<=in1+in2;
+        real_out2<=in1-in2; 
     end
 endmodule
